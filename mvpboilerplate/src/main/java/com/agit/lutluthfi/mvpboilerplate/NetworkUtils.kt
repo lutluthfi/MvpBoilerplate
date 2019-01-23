6d31e22c1a268568227ace32214ea17418c0ab47
Arif Luthfiansyah <lutluthfiansyah@gmail.com>
2019-01-24 05:23:12 +0700

    [arif] Update NetworkUtils

--- mvpboilerplate/src/main/java/com/agit/lutluthfi/mvpboilerplate/NetworkUtils.kt
+++ mvpboilerplate/src/main/java/com/agit/lutluthfi/mvpboilerplate/NetworkUtils.kt
@@ -2,13 +2,13 @@ package com.agit.lutluthfi.mvpboilerplate
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkInfo
+import io.reactivex.Observable
 
 object NetworkUtils {
 
-    fun isNetworkConnected(context: Context): Boolean {
+    fun isNetworkConnected(context: Context): Observable<Boolean> {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val activeNetwork = cm.activeNetworkInfo
-        return activeNetwork != null && activeNetwork.isConnectedOrConnecting
+        return Observable.just(activeNetwork != null && activeNetwork.isConnected)
     }
 }// This utility class is not publicly instantiable
